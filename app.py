0f086dc9f542e4385254f488ea5c90ba3bd92655
csdoge22 <sunnyambashta@gmail.com>
2025-01-28 00:06:26 -0500

    Making Progress

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
+# library used to access directories and the .env file (NOT SHOWN for security reasons)
 import os
+
+# libraries to visualize and organize data
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# library to load environment variables
+# libraries to treat categorical variables within the dataframe
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import OneHotEncoder
+# libraries to train the model
+
+# libraries to load environment variables
 from dotenv import load_dotenv
 
+"""Process data from the data set"""
 load_dotenv()
 BASE_DIR = os.getenv("BASE_DIR")
 diabetes_csv = pd.read_csv(os.path.join(BASE_DIR,"diabetes_prediction_india.csv"))
@@ -13,6 +22,15 @@ diabetes_csv = pd.read_csv(os.path.join(BASE_DIR,"diabetes_prediction_india.csv"
 """TODO: Clean Up Data """
 diabetes_csv = diabetes_csv.drop_duplicates().dropna()
 
-# print(diabetes_csv)
+"""TODO: Treat Categorical Variables """
+
 print(diabetes_csv.dtypes)
+
+veg_le = LabelEncoder()
+
+#diabetes_csv['Encoded Diet Type'] = veg_le.fit_transform(['Diet Type'])
+#print(diabetes_csv)
+
+diabetes_dict = diabetes_csv.to_dict()
+print(diabetes_dict)
 """TODO: Train and Test the Model """
