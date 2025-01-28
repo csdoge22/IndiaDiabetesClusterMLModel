608d15410b6ac644c1a2dae08d221c4122fb3cd4
csdoge22 <sunnyambashta@gmail.com>
2025-01-28 00:17:57 -0500

    Adding some up to date documentation

--- app.py
+++ app.py
@@ -19,6 +19,8 @@ load_dotenv()
 BASE_DIR = os.getenv("BASE_DIR")
 diabetes_csv = pd.read_csv(os.path.join(BASE_DIR,"diabetes_prediction_india.csv"))
 
+"""TODO: Find a Goal for this ML Model"""
+
 """TODO: Clean Up Data """
 diabetes_csv = diabetes_csv.drop_duplicates().dropna()
 
@@ -26,11 +28,9 @@ diabetes_csv = diabetes_csv.drop_duplicates().dropna()
 
 print(diabetes_csv.dtypes)
 
-veg_le = LabelEncoder()
+veg_le, smoking_le, alcohol_le, = LabelEncoder(), LabelEncoder(), LabelEncoder()
 
-#diabetes_csv['Encoded Diet Type'] = veg_le.fit_transform(['Diet Type'])
-#print(diabetes_csv)
+diabetes_csv['Encoded_Diet_Type'] = veg_le.fit_transform(diabetes_csv['Diet_Type'])
+print(diabetes_csv)
 
-diabetes_dict = diabetes_csv.to_dict()
-print(diabetes_dict)
 """TODO: Train and Test the Model """
